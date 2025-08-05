58ca5c5a4b30a8bdda0387a9a59cad4c0d31d74c
周性伟 <zhouxingwei@zhouxineideMini.lan>
2025-08-06 00:40:03 +0800

    6

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,13 +4,13 @@ import './custom.css'
 export default {
   ...DefaultTheme,
   enhanceApp({ router }) {
-    // 当访问根路径时，重定向到文档页面
-    if (typeof window !== 'undefined') {
-      router.onAfterRouteChanged = (to) => {
-        if (to === '/') {
-          window.location.href = '/guide/'
-        }
-      }
-    }
+    // // 当访问根路径时，重定向到文档页面
+    // if (typeof window !== 'undefined') {
+    //   router.onAfterRouteChanged = (to) => {
+    //     if (to === '/') {
+    //       window.location.href = '/guide/'
+    //     }
+    //   }
+    // }
   }
 } 
